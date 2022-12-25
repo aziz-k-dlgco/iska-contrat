d588d5f16801a83216f1cd081dcdede31640ebd0
azk <aziz.kamagate@local.lan>
2022-12-25 10:49:33 +0000

    Cache pour les types de contrat

--- code/src/Controller/Contrat/TypeContratController.php
+++ code/src/Controller/Contrat/TypeContratController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/z/contrat/type-contrat')]
+#[Route('/api/contrat/type-contrat')]
 class TypeContratController extends AbstractController
 {
     #[Route('/', name: 'app_contrat_type_contrat_all', methods: ['GET'])]
@@ -17,7 +17,19 @@ class TypeContratController extends AbstractController
     {
         try {
             $typeContrats = $manager->getRepository(TypeContrat::class)->findAll();
-            return $this->json($typeContrats, Response::HTTP_OK, [], ['groups' => 'read']);
+            return $this->json(
+                array_map(
+                    function (TypeContrat $typeContrat) {
+                        return [
+                            'value' => $typeContrat->getId(),
+                            'label' => $typeContrat->getLib(),
+                        ];
+                    }, $typeContrats),
+                Response::HTTP_OK,
+                [
+                    'Cache-Control' => 'max-age=3600',
+                ],
+            );
         } catch (\Exception $e) {
             return $this->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
