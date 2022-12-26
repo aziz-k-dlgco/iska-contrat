dcc49e1fb417aab2067127d5f25875746e342f29
azk <aziz.kamagate@local.lan>
2022-12-26 07:28:06 +0000

    sonata-project/entity-audit-bundle install

--- code/config/bundles.php
+++ code/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Andante\TimestampableBundle\AndanteTimestampableBundle::class => ['all' => true],
+    SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle::class => ['all' => true],
 ];
